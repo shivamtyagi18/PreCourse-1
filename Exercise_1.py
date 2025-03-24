21075bf2b602a6b914bb7b0840e4134acda20d25
Shivam Tyagi <shivamtyagi18@gmail.com>
2025-03-23 21:08:54 -0500

    PreCource Problem1 and Prblem2 solution

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,34 @@
 class myStack:
   #Please read sample.java file before starting.
+  #Implement Stack using Array
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
+      self.stack = []
          
      def isEmpty(self):
+      return len(self.stack) == 0
          
      def push(self, item):
+      return self.stack.append(item)
          
      def pop(self):
-        
+      return self.stack[:-1]
         
      def peek(self):
+      return self.stack[-1]
         
      def size(self):
+      return len(self.stack)
          
      def show(self):
+      print(self.stack)
          
 
 s = myStack()
 s.push('1')
 s.push('2')
 print(s.pop())
+print(s.peek())
+print(s.size())
+print(s.isEmpty())
 print(s.show())
--- Exercise_2.py
+++ Exercise_2.py
@@ -6,10 +6,21 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.root = Node(None)
         
     def push(self, data):
+        current_node = self.root
+        self.root = Node(data)
+        self.root.next = current_node
+        return self.root   
         
     def pop(self):
+        if self.root.data is None:
+            return None
+        current_head = self.root
+        self.root = Node(current_head.next.data)
+        self.root.next = current_head.next.next
+        return current_head.data
         
 a_stack = Stack()
 while True:
